d16a63a3a2f33ed1d02a1179a1d8fd0ece2a9738
saysa <saysabou@gmail.com>
2015-02-08 19:04:39 +0100

    FOS UerBundle install

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Vyper\SiteBundle\VyperSiteBundle(),
+            new Vyper\UserBundle\VyperUserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
